6591b87493bbb94fdb88ba8a4ec9fafdc103931d
Ирина <kasalot@pt4k.local>
2022-08-29 15:06:47 +0300

    добавили файл

---
+++ Семинар 2/task10/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Write("Введите трехзначное число ");
+int num = int.Parse(Console.ReadLine());
+
+int result = (num / 10) % 10;
+
+Console.WriteLine ($"{num} -> {result}");
---
+++ Семинар 2/task15/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Write("Введите день недели ");
+int day = int.Parse(Console.ReadLine());
+if (5 < day && day < 8) 
+{
+     Console.WriteLine($"{day} -> Да");
+} 
+    else if (0 < day && day < 6)
+     {
+        Console.WriteLine($"{day} -> Нет");
+    }
+else 
+{
+   Console.WriteLine("Такого дня недели нет");
+}
