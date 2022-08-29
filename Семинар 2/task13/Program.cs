f4fe8ed181ea7efea31170608032f742a8c8813c
Ирина <kasalot@pt4k.local>
2022-08-29 16:26:06 +0300

    добавили файл  task15

--- Семинар 2/task13/Program.cs
+++ Семинар 2/task13/Program.cs
@@ -2,4 +2,15 @@
 
 Console.Write("Введите число " );
 int num = int.Parse(Console.ReadLine());
+int num1 = (num/100)%10;
 
+int result = num1;
+
+if (99 > num)
+{
+Console.WriteLine ($"{num} -> {"третьей цифры нет"}");
+}
+else
+{
+    Console.WriteLine($"{num} -> {result}");
+}
