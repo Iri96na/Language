5a64e1d8065912e4c0fbb1f092635bc5ee3dfba5
Ирина <kasalot@pt4k.local>
2022-09-18 23:58:44 +0300

    перенос

---
+++ Семинар 5/task34/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int [] GetArray(int size, int minValue, int maxValue)
+{
+int [] res = new int[size];
+
+for (int i = 0; i < size; i++)
+{
+res[i] = new Random().Next(minValue, maxValue);
+}
+return res;
+}
+
+
+/*int[] evenNumber(int[] array)
+{
+int res = 0;
+for (int i = 0; i > array.Length; i++)
+{
+if (array[i] % 2 == 0)
+res++;
+}
+}
+return res;*/
+
+int[] myArray = GetArray(5, 100, 999);
+Console.WriteLine(String.Join(" ",myArray));
+Console.WriteLine($"{even}");
---
+++ Семинар 5/task36/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+int [] GetArray(int size, int minValue, int maxValue)
+{
+int [] res = new int[size];
+
+for (int i = 0; i < size; i++)
+{
+res[i] = new Random().Next(minValue, maxValue);
+}
+return res;
+}
+int sumElement(int[] array)
+{
+int result = 0;
+int min = 0;
+int max = 0;
+
+if (array[min] > array[max])
+result = array[min] - array[max];
+else if (array[max] > array[min])
+result = array[max] - array[min];
+return result;
+}
+
+
+int[] myArray = GetArray(5, 0, 100);
+Console.WriteLine(String.Join(",",myArray));
+int result = sumElement (myArray);
+Console.WriteLine(String.Join("",result));
